8c57af8f36cc0bb66f406953dce02d09b59c4f39
grant-and-olivia <60475530+grant-and-olivia@users.noreply.github.com>
2020-02-08 15:18:07 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,4 +3,8 @@ $( document ).ready(function() {
         console.log('yo');
         $(this).find('p').css('visibility', 'visible');
     });
+    $('.container').mouseleave(function() {
+        console.log('chicken');
+        $(this).find('p').css('visibility', 'visible');
+    });
 });
