683da079e7298c953e3de01a5a9de436763babeb
grant-and-olivia <60475530+grant-and-olivia@users.noreply.github.com>
2020-02-08 16:26:42 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,12 +3,10 @@ $( document ).ready(function() {
     $('.container').mouseenter(function() {
         $(this).find('p').css('visibility', 'visible');
         $(this).find('img').css('opacity', '0.5');
-        $(this).find('img').css('transform', '1.05');
     });
     $('.container').mouseleave(function() {
         $(this).find('p').css('visibility', 'hidden');
         $(this).find('img').css('opacity', '1.0');
-       $(this).find('img').css('transform', '1.00');
     });
     
     VANTA.TOPOLOGY({
