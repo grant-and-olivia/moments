e70cb8dae6f1012e9ae1699be3a144c945e153ed
grant-and-olivia <60475530+grant-and-olivia@users.noreply.github.com>
2020-02-08 15:43:54 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,4 +7,16 @@ $( document ).ready(function() {
         $(this).find('p').css('visibility', 'hidden');
         $(this).find('img').css('opacity', '1.0');
     });
+    
+    VANTA.TOPOLOGY({
+      el: "#your-element-selector",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0xcf25d,
+      backgroundColor: 0x2a23a0
+    })
 });
