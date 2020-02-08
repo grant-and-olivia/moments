073ccab1f79fda214bc689c7ac7edc947d383377
grant-and-olivia <60475530+grant-and-olivia@users.noreply.github.com>
2020-02-08 14:54:56 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     $('.zoom').hover(function() {
         var id = ($(this).attr("id")).replace('img', 'p');
-        console.log($("#"+id).text());
+        console.log(id);
         $("#"+id).hide();
     });
 });
